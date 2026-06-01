e4d6bc23370775efe327c26009d3a27f1d8e545c
Igor Levchenko <ekithein@gmail.com>
2026-06-01 19:09:02 +0500

    Break lint

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,10 +9,10 @@ class ExampleTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_returns_a_successful_response():void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(500);
+        $response->assertStatus(200);
     }
 }
